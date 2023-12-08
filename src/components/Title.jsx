632e6d0a7d82693773e20ef6beca899f5db393b7
Manish Vangala <manishvangala151@gmail.com>
2023-12-08 11:51:50 +0530

    changes made

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Title.css'
+
+const Title = ({children, ...rest}) => {
+    return (
+    <>
+    <h1>{children}</h1>
+
+
+
+    </>
+    )
+}
+
+export default Title
